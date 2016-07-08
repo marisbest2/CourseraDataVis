4cbb0b6ff84dd6fd4dc412306f68f00c363b8e4e
Michael Rochlin <michaelrochlin@gmail.com>
2016-07-07 23:23:51 -0400

    setting up for heroku deployment

--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const express = require('express');
 const logger = require('morgan');
 const app = express();
 
+app.set('port', (process.env.PORT || 3000));
+
 // Log the requests
 app.use(logger('dev'));
 
 // Serve static files
 app.use(express.static(__dirname)); 
 // app.get('*', (req, res) => res.render(path.join(__dirname,"weather.html")))
+
+app.get('/', (request, response) => response.render('pages/weather.html'););
 
 // Fire it up!
 app.listen(3000);
